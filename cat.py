944a4a68672ffc4ac23a6b7947f2b5c64cd3bf70
Lukas Hagman <Lukas.Hagman@Multisoft.se>
2016-01-22 11:37:20 +0100

    Make cat either display -f(ilepath) or -n(ame). Move previous -n to -b(anner).

--- cat.py
+++ cat.py
@@ -1,15 +1,19 @@
 import sys, lambdatools, os
 
-def show(fpath, longest):
+def show(fpath, fname, longest):
 	global switches;
 	f = open(fpath);
 
-	if '-n' in switches:
+	# banner
+	if '-b' in switches:
 		print('\n=============== ' + fpath + ' ===============');
 
+	# file(path)
 	if '-f' in switches:
-
 		[print(fpath.rjust(longest, ' ') + ": " + line.rstrip('\n')) for line in f];
+	# name
+	if '-n' in switches:
+		[print(fname.rjust(longest, ' ') + ": " + line.rstrip('\n')) for line in f];
 	else:
 		[print(line.rstrip('\n')) for line in f];
 
@@ -27,9 +31,17 @@ if __name__ == "__main__":
 			args.append(line.rstrip());
 
 	args = args.where(lambda a: os.path.isfile(a));
-	longest = len(max(args, key=len));
+
+	cwdlen = len(os.getcwd().replace("\\\\", "/")) + 1 if '-n' in switches else 0; 
+
+	#b = list(arg[cwdlen:] for arg in args);
+	b = [arg[cwdlen:] for arg in args];
+	argfnames = dict(zip(args, b));
+
+	longest = len(max([(v if '-n' in switches else k) for k,v in argfnames.items()], key=len));
 
 	if len(args) > 0:
-		[show(arg, longest) for arg in args];
+		#[show(arg, longest) for arg in args];
+		[show(arg, fname, longest) for arg, fname in argfnames.items()];
 	else:
 		print('USAGE: cat <filename(s)>')
