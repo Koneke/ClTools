5cd5f7daecc19cf9e778e011fee58aa893b27cc1
Lukas Hagman <Lukas.Hagman@Multisoft.se>
2016-01-22 11:48:41 +0100

    cat: -l(ine numbers), cleaning

--- cat.py
+++ cat.py
@@ -8,14 +8,31 @@ def show(fpath, fname, longest):
 	if '-b' in switches:
 		print('\n=============== ' + fpath + ' ===============');
 
+	lefthand = "";
 	# file(path)
 	if '-f' in switches:
-		[print(fpath.rjust(longest, ' ') + ": " + line.rstrip('\n')) for line in f];
+		lefthand = fpath.rjust(longest, ' ');
 	# name
 	if '-n' in switches:
-		[print(fname.rjust(longest, ' ') + ": " + line.rstrip('\n')) for line in f];
-	else:
-		[print(line.rstrip('\n')) for line in f];
+		lefthand = fname.rjust(longest, ' ');
+
+	digits = 0;
+	if '-l' in switches:
+		# Not cheap, but makes for nicer output.
+		# (and I don't think the performance will be an issue really.)
+		digits = len(str(sum(1 for line in open(fpath))));
+		lefthand = lefthand + " {l#}";
+
+	if lefthand != "":
+		lefthand = lefthand + ": ";
+
+	linenumber = 1;
+	for line in f:
+		if '-l' in switches:
+			print(lefthand.replace("{l#}", str(linenumber).rjust(digits, ' ')) + line.rstrip('\n'));
+		else:
+			print(lefthand + line.rstrip('\n'));
+		linenumber = linenumber + 1;
 
 if __name__ == "__main__":
 	global switches;
@@ -34,14 +51,12 @@ if __name__ == "__main__":
 
 	cwdlen = len(os.getcwd().replace("\\\\", "/")) + 1 if '-n' in switches else 0; 
 
-	#b = list(arg[cwdlen:] for arg in args);
 	b = [arg[cwdlen:] for arg in args];
 	argfnames = dict(zip(args, b));
 
 	longest = len(max([(v if '-n' in switches else k) for k,v in argfnames.items()], key=len));
 
 	if len(args) > 0:
-		#[show(arg, longest) for arg in args];
 		[show(arg, fname, longest) for arg, fname in argfnames.items()];
 	else:
 		print('USAGE: cat <filename(s)>')
